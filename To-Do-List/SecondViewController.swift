00e1d161f525626d16a1f01241833bdfbcd639eb
Jigar Parekh <jigarparekh002@gmail.com>
2017-03-30 12:52:55 +0530

    Designed basic UI

---
+++ To-Do-List/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  To-Do-List
+//
+//  Created by Jigar Parekh on 30/03/17.
+//  Copyright © 2017 Next. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var inputTodoText: UITextField!
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
