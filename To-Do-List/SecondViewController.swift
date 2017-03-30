debc7e288000e66b97d2bdf43c92939e23e9313f
Jigar Parekh <jigarparekh002@gmail.com>
2017-03-30 15:44:03 +0530

    Added user defaults and basic app ready

--- To-Do-List/SecondViewController.swift
+++ To-Do-List/SecondViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var inputTodoText: UITextField!
     
     @IBAction func saveButton(_ sender: Any) {
+        let itemObjects = UserDefaults.standard.object(forKey: "items")
+        
+        var items : [String]
+        
+        if let checkObjects = itemObjects as? [String]{
+            items = checkObjects
+            items.append(inputTodoText.text!)
+            
+        }
+        else
+        {
+            items = [inputTodoText.text!]
+        }
+        inputTodoText.text = ""
+        UserDefaults.standard.set(items, forKey: "items")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
